792372997588359951b58108a15161e038f1f76d
JakeCode06 <70036103+JakeCode06@users.noreply.github.com>
2020-08-27 11:51:05 -0300

    Update desempilha.js
    Realizado alteração nos métodos .pop e .push de forma que atualizem o menor número sempre que um novo elemento seja inserido.
    Método getMinNumber(), agora tem a função apenas de mostrar na tela o menor número já definido e caso não possua mais elemento no array, usuário será informado de que a pilha se encontra vazia.

--- desempilha.js
+++ desempilha.js
@@ -1,75 +1,60 @@
 const stack = () => {
-    const date = []
-    let top = -1;
+    const date = [];
+    let top = 0;
+    let minNumber;
 
     const push = (element) => {
         if (element == parseInt(element)) {
-            top++;
-            date[top] = element;
-            return element
+            date[top++] = element;
+            if (minNumber === undefined || element < minNumber) {
+                minNumber = element;
+            }
+            return element;
         } else {
-            return `${element} - Elemento não será inserido no array!`
+            return `${element} - Elemento não inteiro!`
         }
     }
 
     const pop = () => {
-        if (top<0) {
-            return 'Sem elementos para retornar!';
+        if (top <= 0) {
+            return 'Pilha vazia!';
         } else {
-            const elementoRetornado = date[top];
-            date.splice(top);
-            top--;
+            const elementoRetornado = date.splice(--top)[0];
+
+            if (elementoRetornado === minNumber) {
+                minNumber = date[0];
+                for (let cont = 0; cont < date.length; cont++) {
+                    if (date[cont] <= minNumber) {
+                        minNumber = date[cont];
+                    }
+                }        
+            }
+
             return elementoRetornado;
         }
-    }
-
-    function minNumber() {
-        let elemento = date[0]
-        let cont = 0
-        for (cont; cont < date.length; cont++) {
-            if (date[cont] < elemento) {
-                elemento = date[cont];
-                continue;
-            }
-        }
-
-        if (elemento >= 0) {
-            return elemento
-        } else {
-            return 'Pilha vazia'
-        }
-        
     }
 
     return {
         push,
         pop,
-        minNumber,
+        getMinNumber: () => minNumber || 'Pilha vazia',
         date,
     }
 }
 
 const pilha = stack();
 
-console.log('ÁREA DE EMPILHAMENTO!')
-
-console.log(pilha.push(3));
+console.log('EMPILHAMENTO!')
 console.log(pilha.push(9));
-console.log(pilha.push(5.5));
+console.log(pilha.push(5));
 console.log(pilha.push(7));
 console.log(pilha.push(2));
-
-console.log('ÁREA DE DESEMPILHAMENTO!');
-
+console.log(pilha.push(1));
+console.log('DESEMPILHAMENTO!');
 console.log(pilha.pop());
 console.log(pilha.pop());
-
-console.log('Menor número:',pilha.minNumber());
-
+console.log('Menor número:', pilha.getMinNumber());
 console.log(pilha.pop());
 console.log(pilha.pop());
 console.log(pilha.pop());
-
-console.log('Menor número:',pilha.minNumber());
-
-console.log(pilha);
+console.log('Menor número:', pilha.getMinNumber());
